fdb631e37737850d12b89d9525ded38d6a9222a5
Jeffrey Barquero <82740024+JeffreyB04@users.noreply.github.com>
2022-08-26 10:26:58 -0600

    Add files via upload

---
+++ InterfacesUsuario01/src/main/java/jbarquero/gui/Application.java
@@ -0,0 +1,8 @@
+package jbarquero.gui;
+
+public class Application {
+    public static void main(String[] args) {   //psvm para crear la linea
+        System.out.println("Hello world");   //sout
+    }
+
+}
---
+++ InterfacesUsuario01/src/main/java/jbarquero/gui/logica/Empleado.java
@@ -0,0 +1,4 @@
+package jbarquero.gui.logica;
+
+public class Empleado {
+}
